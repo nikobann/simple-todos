43710f2f26e86d524d9f74fb10e588e238401817
nikobann <33771950+nikobann@users.noreply.github.com>
2017-11-18 15:43:20 +0900

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from 'react-apollo';
 import TodoList from './TodoList';
 
 const link = new HttpLink({
-  uri: 'https://api.graph.cool/simple/v1/cj98a57z515m50115c8of9cu7',
+  uri: 'https://api.graphcms.com/simple/v1/cja4wnnwa1w4m01697edo325m',
 });
 const cache = new InMemoryCache();
 const client = new ApolloClient({ link, cache });
